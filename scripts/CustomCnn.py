7d6e6fd8f500271096e5904daa940a43050a0ece
Traxius1 <manolis@savvakis.info>
2024-02-10 21:09:34 +0200

    initial commit

---
+++ scripts/CustomCnn.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CustomCnn(nn.Module):
+    def __init__(self, num_classes=2):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 61 * 61, 64)
+        self.fc2 = nn.Linear(64, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x))
+        return x
